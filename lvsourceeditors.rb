6cec0a574de4762c5bf2099ab455e095a26590b4
zhaofeiyu <zhaofeiyu@lvmama.com>
2017-03-23 14:20:05 +0800

    modify downloadUrl

--- lvsourceeditors.rb
+++ lvsourceeditors.rb
@@ -5,7 +5,7 @@
 class Lvsourceeditors < Formula
   desc "replace #import to @import quote"
   homepage "http://lvioscode.com/ios_support_tools/LvmmSourceEditor.git"
-  url "http://10.200.5.103/ios_support_tools/LvmmSourceEditor/raw/1.0.0/lvrof"
+  url "http://10.200.5.103/ios_support_tools/LvmmSourceEditor/raw/1.0.0/ReplaceOneFramework/lvrof"
   version "1.0.0"
   sha256 "a6277c081641496ed3b9715a8b3e9f27106b995483dbfc68c7f680c3e641230f"
 
